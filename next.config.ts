5be9134f00af148f0f2d85131e7e848013068b16
Munyaradzi Chikara <syntaxread3r@gmail.com>
2025-07-19 12:32:57 +0100

    vr-commit (v1.0.13) - add useRef and update navbar 5

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig: NextConfig = {
   ...(isProd && { output: "export" }),
-  basePath: "",
+  basePath: isProd ? "/hook-lab" : "",
   ...(isProd && { trailingSlash: true }),
   images: {
     unoptimized: isProd,
